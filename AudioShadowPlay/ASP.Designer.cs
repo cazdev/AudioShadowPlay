18af55fe9398f5944fe7d382f6455003a393c3f9
Chaz L <chaz6cl@live.com>
2019-04-22 11:15:47 +1000

    AudioShadowPlay

---
+++ AudioShadowPlay/ASP.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace AudioShadowPlay
+{
+    partial class ASP
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ASP));
+            this.RestartRecording = new System.Windows.Forms.Timer(this.components);
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.pnlRecording = new System.Windows.Forms.Panel();
+            this.pnlSettings = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtFileExpire = new System.Windows.Forms.TextBox();
+            this.btnApply = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTempSave = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnSettings = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblTick = new System.Windows.Forms.Label();
+            this.toolTip = new System.Windows.Forms.ToolTip(this.components);
+            this.pnlSettings.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // RestartRecording
+            // 
+            this.RestartRecording.Interval = 30000;
+            this.RestartRecording.Tick += new System.EventHandler(this.RestartRecording_Tick);
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.btnStart.FlatAppearance.BorderSize = 0;
+            this.btnStart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStart.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.ForeColor = System.Drawing.Color.White;
+            this.btnStart.Location = new System.Drawing.Point(129, 50);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(123, 54);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start ASP";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.btnStop.Enabled = false;
+            this.btnStop.FlatAppearance.BorderSize = 0;
+            this.btnStop.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStop.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStop.ForeColor = System.Drawing.Color.White;
+            this.btnStop.Location = new System.Drawing.Point(129, 241);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(123, 54);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "Stop ASP";
+            this.btnStop.UseVisualStyleBackColor = false;
+            this.btnStop.Click += new System.EventHandler(this.BtnStop_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.btnSave.FlatAppearance.BorderSize = 0;
+            this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSave.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.ForeColor = System.Drawing.Color.White;
+            this.btnSave.Location = new System.Drawing.Point(119, 144);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(143, 63);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Save Audio";
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.BtnSave_Click);
+            // 
+            // pnlRecording
+            // 
+            this.pnlRecording.BackColor = System.Drawing.Color.Maroon;
+            this.pnlRecording.Location = new System.Drawing.Point(368, 12);
+            this.pnlRecording.Name = "pnlRecording";
+            this.pnlRecording.Size = new System.Drawing.Size(20, 20);
+            this.pnlRecording.TabIndex = 3;
+            this.pnlRecording.Visible = false;
+            // 
+            // pnlSettings
+            // 
+            this.pnlSettings.BackColor = System.Drawing.Color.DarkCyan;
+            this.pnlSettings.Controls.Add(this.lblTick);
+            this.pnlSettings.Controls.Add(this.label2);
+            this.pnlSettings.Controls.Add(this.txtFileExpire);
+            this.pnlSettings.Controls.Add(this.btnApply);
+            this.pnlSettings.Controls.Add(this.label1);
+            this.pnlSettings.Controls.Add(this.txtTempSave);
+            this.pnlSettings.Location = new System.Drawing.Point(12, 68);
+            this.pnlSettings.Name = "pnlSettings";
+            this.pnlSettings.Size = new System.Drawing.Size(162, 127);
+            this.pnlSettings.TabIndex = 6;
+            this.pnlSettings.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(3, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 37);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Temp file expire";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.toolTip.SetToolTip(this.label2, "Time before temp files are deleted (default = 120)");
+            // 
+            // txtFileExpire
+            // 
+            this.txtFileExpire.BackColor = System.Drawing.Color.Teal;
+            this.txtFileExpire.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtFileExpire.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFileExpire.ForeColor = System.Drawing.Color.White;
+            this.txtFileExpire.Location = new System.Drawing.Point(76, 52);
+            this.txtFileExpire.Name = "txtFileExpire";
+            this.txtFileExpire.Size = new System.Drawing.Size(75, 23);
+            this.txtFileExpire.TabIndex = 3;
+            this.txtFileExpire.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtFileExpire.TextChanged += new System.EventHandler(this.TxtFileExpire_TextChanged);
+            // 
+            // btnApply
+            // 
+            this.btnApply.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.btnApply.FlatAppearance.BorderSize = 0;
+            this.btnApply.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnApply.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnApply.ForeColor = System.Drawing.Color.White;
+            this.btnApply.Location = new System.Drawing.Point(53, 94);
+            this.btnApply.Name = "btnApply";
+            this.btnApply.Size = new System.Drawing.Size(62, 28);
+            this.btnApply.TabIndex = 2;
+            this.btnApply.Text = "Apply";
+            this.btnApply.UseVisualStyleBackColor = false;
+            this.btnApply.Click += new System.EventHandler(this.BtnApply_Click);
+            // 
+            // label1
+            // 
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(3, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 37);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Temp file length";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.toolTip.SetToolTip(this.label1, "Length of temp audio files (default = 30)");
+            // 
+            // txtTempSave
+            // 
+            this.txtTempSave.BackColor = System.Drawing.Color.Teal;
+            this.txtTempSave.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtTempSave.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTempSave.ForeColor = System.Drawing.Color.White;
+            this.txtTempSave.Location = new System.Drawing.Point(76, 11);
+            this.txtTempSave.Name = "txtTempSave";
+            this.txtTempSave.Size = new System.Drawing.Size(75, 23);
+            this.txtTempSave.TabIndex = 0;
+            this.txtTempSave.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtTempSave.TextChanged += new System.EventHandler(this.TxtTempSave_TextChanged);
+            this.txtTempSave.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TxtTempSave_KeyPress);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::AudioShadowPlay.Properties.Resources.Speaker_512;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(346, 296);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(42, 42);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnSettings
+            // 
+            this.btnSettings.BackgroundImage = global::AudioShadowPlay.Properties.Resources.Settings_01;
+            this.btnSettings.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnSettings.FlatAppearance.BorderSize = 0;
+            this.btnSettings.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DarkCyan;
+            this.btnSettings.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DarkCyan;
+            this.btnSettings.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSettings.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSettings.ForeColor = System.Drawing.Color.White;
+            this.btnSettings.Location = new System.Drawing.Point(12, 12);
+            this.btnSettings.Name = "btnSettings";
+            this.btnSettings.Size = new System.Drawing.Size(53, 50);
+            this.btnSettings.TabIndex = 5;
+            this.btnSettings.UseVisualStyleBackColor = false;
+            this.btnSettings.Click += new System.EventHandler(this.BtnSettings_Click);
+            // 
+            // label3
+            // 
+            this.label3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(313, 321);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "v1.0";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // lblTick
+            // 
+            this.lblTick.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblTick.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTick.ForeColor = System.Drawing.Color.Lime;
+            this.lblTick.Location = new System.Drawing.Point(118, 93);
+            this.lblTick.Name = "lblTick";
+            this.lblTick.Size = new System.Drawing.Size(41, 28);
+            this.lblTick.TabIndex = 9;
+            this.lblTick.Text = "✔️";
+            this.lblTick.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblTick.Visible = false;
+            // 
+            // ASP
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Teal;
+            this.ClientSize = new System.Drawing.Size(400, 350);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pnlSettings);
+            this.Controls.Add(this.btnSettings);
+            this.Controls.Add(this.pnlRecording);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "ASP";
+            this.Text = "AudioShadowPlay";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ASP_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.ASP_Click);
+            this.pnlSettings.ResumeLayout(false);
+            this.pnlSettings.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer RestartRecording;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Panel pnlRecording;
+        private System.Windows.Forms.Panel pnlSettings;
+        private System.Windows.Forms.Button btnSettings;
+        private System.Windows.Forms.TextBox txtTempSave;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnApply;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtFileExpire;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblTick;
+        private System.Windows.Forms.ToolTip toolTip;
+    }
+}
+
---
+++ AudioShadowPlay/ASP.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using NAudio.Wave;
+using System.IO;
+
+namespace AudioShadowPlay
+{
+    public partial class ASP : Form
+    {
+        private static bool SaveNext = false;
+        private static int TimeBeforeCleanUp = 120;
+        private static int FileCount = 0;
+        
+        private static string outputPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + $@"\ASP\Temp\";
+        private static string savedPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + $@"\ASP\SavedAudio\";
+        private static string configPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + $@"\ASP\config.asp";
+
+        private WasapiLoopbackCapture CaptureInstance = null;
+        private WaveFileWriter RecordedAudioWriter = null;
+
+        public ASP()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            System.IO.FileInfo file1 = new System.IO.FileInfo(outputPath);
+            file1.Directory.Create(); // If the directory already exists, this method does nothing.
+
+            System.IO.FileInfo file2 = new System.IO.FileInfo(savedPath);
+            file2.Directory.Create(); // If the directory already exists, this method does nothing.
+
+            if (!File.Exists(configPath))
+            {
+                FileStream fs = File.Create(configPath);
+                fs.Dispose();
+            }
+
+            txtTempSave.Text = "30";
+            txtFileExpire.Text = "120";
+
+            StreamReader sr = new StreamReader(configPath);
+            string contents = sr.ReadToEnd();
+            foreach (string line in File.ReadLines(configPath))
+            {
+                if (line.Contains("TempSaveInterval = "))
+                {
+                    txtTempSave.Text = line.Substring(18, line.Length - 18);
+                    RestartRecording.Interval = Int16.Parse(line.Substring(18, line.Length - 18)) * 1000;
+                }
+
+                if (line.Contains("TempFileExpire = "))
+                {
+                    txtFileExpire.Text = line.Substring(17, line.Length - 17);
+                    TimeBeforeCleanUp = Int16.Parse(line.Substring(17, line.Length - 17));
+                }
+            }
+            sr.Dispose();
+        }
+
+        private void StartRecording()
+        {
+            string outputFilePath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + $@"\ASP\Temp\recorded_audio_{FileCount}.wav";
+            
+            // Redefine the capturer instance with a new instance of the LoopbackCapture class
+            this.CaptureInstance = new WasapiLoopbackCapture();
+
+            // Redefine the audio writer instance with the given configuration
+            this.RecordedAudioWriter = new WaveFileWriter(outputFilePath, CaptureInstance.WaveFormat);
+
+            // When the capturer receives audio, start writing the buffer into the mentioned file
+            this.CaptureInstance.DataAvailable += (s, a) =>
+            {
+                this.RecordedAudioWriter.Write(a.Buffer, 0, a.BytesRecorded);
+            };
+
+            CaptureInstance.StartRecording();
+
+            pnlRecording.Visible = true;
+
+            btnStart.Enabled = false;
+            btnStop.Enabled = true;
+
+            pnlRecording.Visible = true;
+
+            RestartRecording.Start();
+        }
+
+        private void StopRecording()
+        {
+            RestartRecording.Stop();
+
+            if (CaptureInstance != null && RecordedAudioWriter != null)
+            {
+                // Stop audio recording
+                CaptureInstance.StopRecording();
+                this.RecordedAudioWriter.Dispose();
+                this.RecordedAudioWriter = null;
+                CaptureInstance.Dispose();
+            }
+
+            FileCount++;
+
+            pnlRecording.Visible = false;
+
+            btnStart.Enabled = true;
+            btnStop.Enabled = false;
+
+            if (SaveNext)
+            {
+                int outRate = 16000;
+                string inFile = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + $@"\ASP\Temp\recorded_audio_{FileCount - 1}.wav";
+                string outFile = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + $@"\ASP\SavedAudio\recorded_audio_save_{FileCount}.wav";
+
+                using (MediaFoundationReader reader = new MediaFoundationReader(inFile))
+                {
+                    WaveFormat outFormat = new WaveFormat(outRate, reader.WaveFormat.Channels);
+                    using (MediaFoundationResampler resampler = new MediaFoundationResampler(reader, outFormat))
+                    {
+                        // resampler.ResamplerQuality = 60;
+                        WaveFileWriter.CreateWaveFile(outFile, resampler);
+                    }
+                }
+
+                SaveNext = false;
+            }
+        }
+
+        private void BtnStop_Click(object sender, EventArgs e)
+        {
+            StopRecording();
+
+            btnSettings.BackColor = Color.Teal;
+            pnlSettings.Visible = false;
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            StartRecording();
+            RestartRecording.Start();
+
+            btnSettings.BackColor = Color.Teal;
+            pnlSettings.Visible = false;
+        }
+
+        private void BtnSave_Click(object sender, EventArgs e)
+        {
+            int outRate = 16000;
+            string inFile = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + $@"\ASP\Temp\recorded_audio_{FileCount-1}.wav";
+            string outFile = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + $@"\ASP\Saves\recorded_audio_save_{FileCount}.wav";
+
+            try
+            {
+                using (MediaFoundationReader reader = new MediaFoundationReader(inFile))
+                {
+                    WaveFormat outFormat = new WaveFormat(outRate, reader.WaveFormat.Channels);
+                    using (MediaFoundationResampler resampler = new MediaFoundationResampler(reader, outFormat))
+                    {
+                        // resampler.ResamplerQuality = 60;
+                        WaveFileWriter.CreateWaveFile(outFile, resampler);
+                    }
+                }
+            }
+            catch { }
+
+            SaveNext = true;
+
+            btnSettings.BackColor = Color.Teal;
+            pnlSettings.Visible = false;
+        }
+
+        private void ASP_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            StopRecording();
+
+            Environment.Exit(0);
+        }
+
+        private void RestartRecording_Tick(object sender, EventArgs e)
+        {
+            StopRecording();
+
+            StartRecording();
+
+            //Clean up
+            System.IO.DirectoryInfo di = new DirectoryInfo(outputPath);
+
+            foreach (FileInfo file in di.GetFiles())
+            {
+                if (file.CreationTime < DateTime.Now.AddSeconds(-TimeBeforeCleanUp))
+                {
+                    file.Delete();
+                }
+            }
+        }
+
+        private void BtnSettings_Click(object sender, EventArgs e)
+        {
+            if (pnlSettings.Visible)
+            {
+                btnSettings.BackColor = Color.Teal;
+                pnlSettings.Visible = false;
+            }
+            else
+            {
+                btnSettings.BackColor = Color.DarkCyan;
+                pnlSettings.Visible = true;
+            }
+        }
+
+        private void BtnApply_Click(object sender, EventArgs e)
+        {
+            RestartRecording.Interval = Int16.Parse(txtTempSave.Text) * 1000;
+            TimeBeforeCleanUp = Int16.Parse(txtFileExpire.Text);
+
+            lblTick.Visible = true;
+
+            WriteToConfig();
+        }
+
+        private void WriteToConfig()
+        {
+            File.WriteAllText(configPath, "");
+            string line1 = $"TempSaveInterval = {RestartRecording.Interval / 1000}" + Environment.NewLine;
+            File.AppendAllText(configPath, line1) ;
+            string line2 = $"TempFileExpire = {TimeBeforeCleanUp}" + Environment.NewLine;
+            File.AppendAllText(configPath, line2);
+            string line3 = Environment.NewLine + "--------------" + Environment.NewLine + Environment.NewLine;
+            File.AppendAllText(configPath, line3);
+            string line4 = "//Length of temp audio files (default = 30)" + Environment.NewLine;
+            File.AppendAllText(configPath, line4);
+            string line5 = "//TempFileExpire: Time before temp files are deleted (default = 120)" + Environment.NewLine;
+            File.AppendAllText(configPath, line5);
+        }
+
+        private void TxtTempSave_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void TxtTempSave_TextChanged(object sender, EventArgs e)
+        {
+            lblTick.Visible = false;    
+        }
+
+        private void TxtFileExpire_TextChanged(object sender, EventArgs e)
+        {
+            lblTick.Visible = false;
+        }
+
+        private void ASP_Click(object sender, EventArgs e)
+        {
+            btnSettings.BackColor = Color.Teal;
+            pnlSettings.Visible = false;
+        }
+    }
+}
